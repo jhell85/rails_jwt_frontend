b341a852d7831e769e21f6dc8fb7bd38837a1044
jhell85 <jhell85@gmail.com>
2020-10-26 19:59:24 -0700

    React front end for logging into Rails backend using JWT for log in authentication

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import Header from './Header'
+import SignInForm from './SignInForm';
+import LoginForm from './LoginForm'
+
+function App() {
+  const [user, setUser] = useState({})
+  const [form, setForm] = useState("")
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    if(token){
+      fetch(`http://localhost:3000/auto_login`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(resp => resp.json())
+      .then(data => {
+        setUser(data)
+        // console.log(data)
+      })
+    }
+  }, [])
+
+  const handleLogin = (user) => {
+    setUser(user)
+  }
+
+  const handleFormSwitch = (input) => {
+    setForm(input)
+  }
+
+  const handleAuthClick = () => {
+    const token = localStorage.getItem("token")
+    fetch(`http://localhost:3000/user_is_authed`, {
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
+
+  console.log(user)
+
+  const handleLogOut = () => {
+    localStorage.clear();
+    setUser({})
+  }
+
+  const handleTestRoute = () =>{
+    const token = localStorage.getItem("token")
+    fetch('http://localhost:3000/test', {
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
+
+  const renderForm = () => {
+    switch(form){
+      case "login":
+        return <LoginForm handleLogin={handleLogin}/>
+        break;
+      default:
+        return <SignInForm handleLogin={handleLogin}/>
+    }
+  }
+  return (
+    <div className="App">
+        <Header handleFormSwitch={handleFormSwitch}/>
+        {
+          renderForm()
+        }
+        <button onClick={handleAuthClick} className="ui button">Access Authorized Route</button>
+        <button onClick={handleLogOut} className='ui button' >log out</button>
+        <button onClick={handleTestRoute} className='ui button' >get Test route</button>
+    </div>
+  );
+}
+
+export default App;
